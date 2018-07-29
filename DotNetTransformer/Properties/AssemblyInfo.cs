567ccc3dc8b5fc8db3d714dd43fb558a38f568bf
leofun01 <leofun01@hotmail.com>
2018-07-29 16:28:05 +0300

    Add file AssemblyInfo.cs

---
+++ DotNetTransformer/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+// AssemblyInfo.cs
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetTransformer")]
+[assembly: AssemblyDescription("Transformations class library for .NET projects.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetTransformer")]
+[assembly: AssemblyCopyright("Copyright © leofun01 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02d87708-8e86-40e8-8be5-35ddef6292ed")]
+
+// Version information for an assembly consists of the following four values:
+//		Major Version
+//		Minor Version 
+//		Build Number
+//		Revision
+[assembly: AssemblyVersion("0.8.0.1")]
+[assembly: AssemblyFileVersion("0.8.0.1")]
