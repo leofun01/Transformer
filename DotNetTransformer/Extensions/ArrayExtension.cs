54637867b270f6bc1fc5182dae92ff6815631cab
leofun01 <leofun01@hotmail.com>
2018-07-29 16:17:18 +0300

    Add extensions for 2D array
    Flip & Rotate 2D array.

---
+++ DotNetTransformer/Extensions/ArrayExtension.cs
@@ -0,0 +1,71 @@
+//	ArrayExtension.cs
+//	
+//	Based on :
+//		.Net
+//			System.Array
+//	
+//	Author   : leofun01
+//	Created  : 2018-07-25
+//	Modified : 2018-07-28
+
+using RotateFlipType = System.Drawing.RotateFlipType;
+using FlipRotate2d = DotNetTransformer.Math.Group.FlipRotate2d;
+
+namespace DotNetTransformer.Extensions {
+	public static class ArrayExtension
+	{
+		public static T[,] Transform<T>(this T[,] array, FlipRotate2d transformation) {
+			if(array == null) return null;
+			byte t = transformation.Value;
+			int dim = t >> 2;
+			int w = array.GetLength(dim);
+			int h = array.GetLength(dim ^ 1);
+			int dx = (t >> 1 ^ t) & 1, dy = t & 1;
+			int ax_init = dx * (w - 1); dx = 1 - (dx << 1);
+			int ay_init = dy * (h - 1); dy = 1 - (dy << 1);
+			T[,] result = new T[w, h];
+			int[] a = new int[2];
+			a[dim ^ 1] = ax_init;
+			for(int rx = 0; rx < w; ++rx, a[dim ^ 1] += dx) {
+				a[dim] = ay_init;
+				for(int ry = 0; ry < h; ++ry, a[dim] += dy)
+					result[rx, ry] = array[a[1], a[0]];
+			}
+			return result;
+		}
+		public static T[,] Transform<T>(this T[,] array, RotateFlipType transformation) {
+			return Transform(array, FlipRotate2d.FromRotateFlipType(transformation));
+		}
+		/*/
+		private static T[,] Transform<T>(T[,] array, int dimension, int dx, int dy) {
+			if(array == null) return null;
+			dimension &= 1;
+			dx &= 1;
+			dy &= 1;
+			int w = array.GetLength(dimension);
+			int h = array.GetLength(dimension ^ 1);
+			int ax_init = dx * (w - 1);
+			int ay_init = dy * (h - 1);
+			dx = 1 - (dx << 1);
+			dy = 1 - (dy << 1);
+			T[,] result = new T[w, h];
+			int[] a = new int[2];
+			a[dimension ^ 1] = ax_init;
+			for(int rx = 0; rx < w; ++rx, a[dimension ^ 1] += dx) {
+				a[dimension] = ay_init;
+				for(int ry = 0; ry < h; ++ry, a[dimension] += dy)
+					result[rx, ry] = array[a[1], a[0]];
+			}
+			return result;
+		}
+		public static T[,] Transform<T>(this T[,] array, FlipRotate2d transformation) {
+			int t = transformation.Value;
+			return Transform(array, t >> 2, (t >> 1 ^ t), t);
+		}
+		public static T[,] Transform<T>(this T[,] array, RotateFlipType transformation) {
+			int t = (int)transformation;
+			return Transform(array, t, ((t >> 2) ^ (t >> 1) ^ t), t >> 1);
+		}
+		//*/
+	}
+}
