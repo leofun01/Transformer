ac4ed70cff259e265c4c6a01377e434be5886245
leofun01 <leofun01@hotmail.com>
2018-07-29 15:07:24 +0300

    Add FiniteSet & EmptySet
    FiniteSet is abstract enumerable set.
    EmptySet is finite set without any elements inside.

---
+++ DotNetTransformer/Math/Set/EmptySet.cs
@@ -0,0 +1,30 @@
+//	EmptySet.cs
+//	
+//	Based on :
+//		Math
+//			Set theory
+//	
+//	Author   : leofun01
+//	Created  : 2018-07-02
+//	Modified : 2018-07-15
+
+using System;
+using System.Collections.Generic;
+
+namespace DotNetTransformer.Math.Set {
+	public abstract partial class FiniteSet<T> : ISet<T>, IEnumerable<T>
+		where T : IEquatable<T>
+	{
+		public static readonly FiniteSet<T> Empty = new EmptySet();
+		private sealed class EmptySet : FiniteSet<T>
+		{
+			public EmptySet() { }
+
+			//public override bool IsEmpty { get { return true; } }
+			public override int Count { get { return 0; } }
+			public override bool Contains(T item) { return false; }
+			public override IEnumerator<T> GetEnumerator() { yield break; }
+			public override bool IsSubsetOf(ISet<T> other) { return true; }
+		}
+	}
+}
