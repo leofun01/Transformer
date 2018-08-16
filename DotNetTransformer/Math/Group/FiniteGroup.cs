3aff9fa0563f6cfc2f1296278fac7a7015c6c18a
leofun01 <leofun01@hotmail.com>
2018-08-16 17:43:38 +0300

    Revert "Merge branch 'develop' into draft"
    This reverts commit 3f4f38a02fdeba3b78ecd1df0d69e4aa0cb2a0c5, reversing
    changes made to fb6b10befbbc18247edf70b61b79caf806f9069c.

--- DotNetTransformer/Math/Group/FiniteGroup.cs
+++ DotNetTransformer/Math/Group/FiniteGroup.cs
@@ -1,3 +1,15 @@
+//	FiniteGroup.cs
+//	
+//	Based on :
+//		Math
+//			Abstract algebra
+//				Group theory
+//					Finite group
+//	
+//	Author   : leofun01
+//	Created  : 2018-06-24
+//	Modified : 2018-07-02
+
 using DotNetTransformer.Math.Set;
 
 namespace DotNetTransformer.Math.Group {
@@ -5,5 +17,7 @@ namespace DotNetTransformer.Math.Group {
 		where T : IFiniteGroupElement<T>
 	{
 		public abstract T IdentityElement { get; }
+		//public virtual bool IsCyclic { get { return Exist(e => e.CycleLength == Count); } }
+		//public override bool IsEmpty { get { return false; } }
 	}
 }
