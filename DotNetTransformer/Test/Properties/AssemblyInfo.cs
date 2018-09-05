d6da6df302e66800d1725eeb2d115fc412412773
leofun01 <leofun01@hotmail.com>
2018-09-05 11:00:21 +0300

    Add AssemblyInfo.cs

---
+++ DotNetTransformer/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+//	AssemblyInfo.cs
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("Tests for DotNetTransformer.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © leofun01 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10c7802e-080d-4b00-a3c7-d33bc9e47b97")]
+
+// Version information for an assembly consists of the following four values:
+//		Major Version
+//		Minor Version 
+//		Build Number
+//		Revision
+[assembly: AssemblyVersion("0.8.0.1")]
+[assembly: AssemblyFileVersion("0.8.0.1")]
