2b206d06ca61dc400460a88004dbc06c1243986a
leofun01 <leofun01@hotmail.com>
2018-09-05 11:16:46 +0300

    Revert "Revert "Merge branch 'develop' into draft""
    This reverts commit 6bb55976bd73ad065ff86882b44ef4ce3a6129d5.

--- DotNetTransformer/Math/Set/ISubSet.cs
+++ DotNetTransformer/Math/Set/ISubSet.cs
@@ -1,27 +1,10 @@
-//	ISubSet.cs
-//	
-//	Based on :
-//		Math
-//			Set theory
-//	
-//	Author   : leofun01
-
 using System;
 
 namespace DotNetTransformer.Math.Set {
-	// T    is contravariant
-	// TSet is contravariant
 	public interface ISubSet<in T, in TSet> : ISet<T>
 		where T    : IEquatable<T>
 		where TSet : ISet<T>
 	{
 		bool IsSubsetOf(TSet other);
 	}
-	/*//
-	public interface ISubSet<in T, in TSubSet, in TSuperSet> : ISet<T>, ISubSet<T, TSuperSet>
-		where T : IEquatable<T>
-		where TSubSet : ISubSet<T, TSubSet, TSuperSet>
-		where TSuperSet : ISuperSet<T, TSubSet, TSuperSet>
-	{ }
-	//*/
 }
