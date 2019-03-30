ede5dce22b3caf677bfa60952d07344ab8a96dcd
leofun01 <leofun01@gmail.com>
2019-03-31 00:24:43 +0200

    Update EnumerableExtension.cs
    - Add `Exist` & `All` extension methods for `IEnumerable<T>`.

--- DotNetTransformer/Extensions/EnumerableExtension.cs
+++ DotNetTransformer/Extensions/EnumerableExtension.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using RotateFlipType = System.Drawing.RotateFlipType;
 using FlipRotate2d = DotNetTransformer.Math.Group.FlipRotate2d;
@@ -5,6 +6,16 @@ using FlipRotate2d = DotNetTransformer.Math.Group.FlipRotate2d;
 namespace DotNetTransformer.Extensions {
 	public static class EnumerableExtension
 	{
+		public static bool Exist<T>(this IEnumerable<T> collection, Predicate<T> match) {
+			foreach(T item in collection)
+				if(match(item))
+					return true;
+			return false;
+		}
+		public static bool All<T>(this IEnumerable<T> collection, Predicate<T> match) {
+			return !collection.Exist<T>(e => !match(e));
+		}
+
 		private delegate T Func<T>(T l, T r);
 		private static T CollectAll<T>(this IEnumerable<T> collection, Func<T> func) where T : struct {
 			T result = default(T);
