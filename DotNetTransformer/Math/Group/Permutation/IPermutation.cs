57d80ebf3b764d2411b68ab9f649cbf64dbf6840
leofun01 <leofun01@gmail.com>
2019-04-22 07:04:24 +0300

    Update files { IPermutation.cs, PermutationByte.cs, PermutationInt64.cs }

--- DotNetTransformer/Math/Group/Permutation/IPermutation.cs
+++ DotNetTransformer/Math/Group/Permutation/IPermutation.cs
@@ -1,3 +1,14 @@
+//	IPermutation.cs
+//	
+//	Based on :
+//		Math
+//			Abstract algebra
+//				Group theory
+//					Finite group
+//					Permutation group
+//	
+//	Author   : leofun01
+
 using System.Collections.Generic;
 
 namespace DotNetTransformer.Math.Group.Permutation {
