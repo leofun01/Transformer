ba417c5e84f156ee61240ed8b7228c68288f9df0
leofun01 <leofun01@gmail.com>
2019-06-11 21:35:22 +0300

    Update Math.Group

--- DotNetTransformer/Math/Group/IFiniteGroupElement.cs
+++ DotNetTransformer/Math/Group/IFiniteGroupElement.cs
@@ -1,3 +1,13 @@
+//	IFiniteGroupElement.cs
+//	
+//	Based on :
+//		Math
+//			Abstract algebra
+//				Group theory
+//					Finite group
+//	
+//	Author   : leofun01
+
 namespace DotNetTransformer.Math.Group {
 	public interface IFiniteGroupElement<T> : IGroupElement<T>
 		where T : IFiniteGroupElement<T>
