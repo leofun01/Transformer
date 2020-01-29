64e6d3947a14171a77036ed5676153544b49404f
leofun01 <leofun01@gmail.com>
2020-01-29 19:15:44 +0200

    Update `Delegate.cs`
    Add delegates :
      - Constructor
      - Generator

--- DotNetTransformer/Extensions/Delegate.cs
+++ DotNetTransformer/Extensions/Delegate.cs
@@ -1,5 +1,10 @@
 namespace DotNetTransformer.Extensions {
+	public delegate T Generator<T>(T arg);
+
 	public delegate bool Order<in T>(T l, T r);
+
 	public delegate T Func<T>(T l, T r);
 	public delegate TOut Func<in TIn, out TOut>(TIn l, TIn r);
+
+	public delegate TOut Constructor<in TIn0, in TIn1, out TOut>(TIn0 arg0, TIn1 arg1);
 }
