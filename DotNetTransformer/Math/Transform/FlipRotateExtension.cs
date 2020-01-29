dae74e543086b7672b4eb10e30cc1aa6e9e7e1d0
leofun01 <leofun01@gmail.com>
2020-01-29 20:37:06 +0200

    Add `GetAllValues` method. Update `FlipRotateExtension.cs`.

--- DotNetTransformer/Math/Transform/FlipRotateExtension.cs
+++ DotNetTransformer/Math/Transform/FlipRotateExtension.cs
@@ -1,9 +1,21 @@
+using System.Collections.Generic;
+using DotNetTransformer.Extensions;
 using DotNetTransformer.Math.Group;
 using DotNetTransformer.Math.Permutation;
 
 namespace DotNetTransformer.Math.Transform {
 	public static class FlipRotateExtension
 	{
+		public static IEnumerable<T> GetAllValues<T, P>(int dim, Constructor<P, int, T> ctor)
+			where T : IFlipRotate<T, P>, new()
+			where P : IPermutation<P>, new()
+		{
+			int c = 1 << dim;
+			P i = new P();
+			foreach(P p in i.GetRange<P>(i, dim))
+				for(int v = 0; v < c; ++v)
+					yield return ctor(p, v);
+		}
 		public static int GetCycleLength<T, P>(this T _this)
 			where T : IFlipRotate<T, P>, new()
 			where P : IPermutation<P>, new()
